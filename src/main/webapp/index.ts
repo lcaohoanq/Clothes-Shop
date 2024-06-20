8de32d5be9835e4c6b54054f9604d59df5dd3979
Hoang Cao Luu <136492579+lcaohoanq@users.noreply.github.com>
2024-06-20 10:19:52 +0700

    refactor: add typescript code (#24)

---
+++ src/main/webapp/index.ts
@@ -0,0 +1,12 @@
+require("dotenv").config({ path: "../../../.env" });
+
+/*
+DB_URL=jdbc:sqlserver://localhost:1433;DatabaseName=ClothesShop
+DB_USER=sa
+DB_PASSWORD=12345
+GOOGLE_CLIENT_ID=715672619045-to8nh4njgffig5goq2de0ases1t0hgho.apps.googleusercontent.com
+GOOGLE_REDIRECT_URI=http://localhost:8080/ClothesShop/LoginServlet
+GOOGLE_LINK_GET_TOKEN=https://accounts.google.com/o/oauth2/token
+GOOGLE_LINK_GET_USER_INFO=https://www.googleapis.com/oauth2/v1/userinfo?access_token=
+GOOGLE_GRANT_TYPE=authorization_code
+*/
